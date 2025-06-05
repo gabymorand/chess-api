c44bc9e0b57766197edb04ee879f05305c182cc9
Morand gabriel <gabriel.morand@brest-opencampus.com>
2025-06-05 13:24:26 +0200

    add cors, oupsi

--- src/main/java/com/chesslearning/chess_api/config/SecurityConfig.java
+++ src/main/java/com/chesslearning/chess_api/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,11 +31,18 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+    
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                
+                // AJOUT CORS POUR SWAGGER
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                
                 .authorizeHttpRequests(auth -> auth
                         // ===== RÈGLES ABSOLUMENT PRIORITAIRES =====
                         .requestMatchers("/api/auth/**").permitAll()
